5fb69ddefc9bd70f5aa3b21373e376c8d88e2b58
mikexsus <160156917+mikexsus@users.noreply.github.com>
2024-03-01 20:51:21 +0400

    Add files via upload

---
+++ Day2/homework.py/homework.py
@@ -0,0 +1,3 @@
+print("GOA programming is good, because i learn how to type codes and all other stuf.")
+print("and i met dimitri, he helped me with my homeworks and he is a kind person.")
+print("if i could rate GOA programming i would give it a 10/10.")
